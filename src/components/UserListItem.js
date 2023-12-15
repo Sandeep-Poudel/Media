effb058f1f46da7d91424f82c67c98a7f87b5a43
Sandeep-Poudel <poudelsandeep0105@gmail.com>
2023-12-15 16:23:02 +0545

    Added expandable component

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,25 +1,30 @@
+import useThunk from "../hooks/useThunk";
+import { Fragment } from "react";
 import { GoTrashcan } from "react-icons/go";
-import Button from "./Button";
 import { removeUser } from "../store";
-import  useThunk from "../hooks/useThunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
+import Button from "./Button";
 
 function UserListItem({ user }) {
     const [doRemoveUser, isDeleting, error] = useThunk(removeUser);
-    const handleClick=()=>{
+    const handleClick = () => {
         doRemoveUser(user);
     }
 
-    return <div className='mb-2 border rounded'>
-        <div className='flex p-2 justify-between items-center cursor-pointer'>
-            <div className="flex flex-row justify-between items-center"> 
+    const header = <Fragment>
+        <Button loading={isDeleting} onClick={handleClick} className="mr-3" >
+            <GoTrashcan />
+        </Button>
+        {error && <div>Error deleting user.</div>}
+        {user.name}
+    </Fragment>
+    return (
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user} />
+        </ExpandablePanel>
+    )
 
-            <Button loading={isDeleting} onClick={handleClick} className="mr-3">
-                <GoTrashcan/>
-            </Button>
-            {error && <div>Error deleting user.</div>}
-            {user.name}
-            </div>
-        </div>
-    </div>
+
 };
 export default UserListItem;
